35a8c169608242b2b8bc499a9820c59e62c1bc59
nikki93 <s.nikhilesh@gmail.com>
2012-10-30 16:55:27 -0400

    Added Skiena 4.18: red/white/blue linear sort

---
+++ skiena/4.18-redwhiteblue.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+
+using namespace std;
+
+// 'r', 'w' or 'b'
+int examine(char **a, int i)
+{
+    return *a[i];
+}
+
+void swap(char **a, int i, int j)
+{
+    char *t = a[i];
+    a[i] = a[j];
+    a[j] = t;
+}
+
+// rwb sort using only 'examine' and 'swap'
+void rwbsort(char **a, int n)
+{
+    int i = 0, l = 0, h = n - 1;
+
+    // invariant: 
+    //    <l:      'r'
+    //    >l, <i:  'w'
+    //    >i, <h:  unknown
+    //    >h:      'b'
+
+    // either i increases or h decreases so linear
+
+    while (i <= h)
+        switch (examine(a, i))
+        {
+            case 'r':
+                swap(a, i++, l++);
+                break;
+
+            case 'w':
+                ++i;
+                break;
+
+            case 'b':
+                swap(a, i, h--);
+                break;
+        }
+}
+
+int main(int argc, char **argv)
+{
+    if (argc < 2)
+    {
+        cout << "usage: " << argv[0] << " r b w w ... "
+            << endl;
+        return 0;
+    }
+
+    // sort
+    rwbsort(argv + 1, argc - 1);
+
+    // print
+    cout << "sorted:";
+    while (--argc)
+        cout << ' ' << *++argv;
+    cout << endl;
+
+    return 0;
+}
